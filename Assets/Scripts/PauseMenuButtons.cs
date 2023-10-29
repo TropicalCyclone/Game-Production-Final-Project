ae90a7f2741a83a2c19d7824a8bc971983e65581
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-10-29 16:32:36 +0800

    Compiled-Player - Completed Compilation of player assets

--- Assets/Scripts/PauseMenuButtons.cs
+++ Assets/Scripts/PauseMenuButtons.cs
@@ -6,23 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenuButtons : MonoBehaviour
 {
+    [SerializeField] private GameObject menu;
     [SerializeField] private FadeScreen fader;
+    [SerializeField] private float Delay = 1;
     [SerializeField] private string mSceneName = "Main Menu";
     public void RestartGame()
     {
         StartCoroutine(FadeOutandRestart());
-        
     }
     public void ExitGame()
     {
+        
         fader.FadeOut();
         SceneManager.LoadScene(mSceneName);
     }
 
     IEnumerator FadeOutandRestart()
     {
+        menu.SetActive(false);
         fader.FadeOut();
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(Delay);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
