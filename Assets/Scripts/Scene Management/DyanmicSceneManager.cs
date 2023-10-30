e84552f72ce34864478509263261550cdb922b13
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-10-30 17:50:18 +0800

    Dyanamic-Scene-Manager - Renamed every level - Created GameScene

---
+++ Assets/Scripts/Scene Management/DyanmicSceneManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DyanmicSceneManager : MonoBehaviour
+{
+    public static DyanmicSceneManager instance { get; private set; }  
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        Load("Compiled-Player");
+        Load("Stairwell");
+        Load("Floor-1");
+        Load("Floor-3");
+        Load("Floor-4");
+        Load("Floor-5");
+        Load("Floor-6");
+        Load("Floor-7");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Load(string sceneName)
+    {
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+    }
+
+    public void Unload(string sceneName)
+    {
+        if (SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
+        }
+    }
+}
