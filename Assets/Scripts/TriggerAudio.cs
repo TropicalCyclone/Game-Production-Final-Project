076f93d4099efd107a0b7ff76b24533c8d467876
Yeshuwa <133879029+4nythng@users.noreply.github.com>
2023-11-08 03:37:19 +0800

    - Added 7th Floor Scene (LDD Requirements)
    I added:
    > 7th Floor Game Scene
    > Scripts
      > Key Item
      > Trigger Audio
      > Trigger Audio Hint
      > Trigger Events
    > Models
      > Key Items Placeholder
      > Disabled Item Placeholder
    > Audio
      > Audio Hint Placeholder (Whisper.mp3)

---
+++ Assets/Scripts/TriggerAudio.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    public AudioClip TriggerSound;
+    public float Volume = 0.5f;
+
+    [HideInInspector]
+    private bool isPlayed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isPlayed)
+        {
+            if (TriggerSound)
+            {
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+
+                if (!audioSource)
+                {
+                    audioSource = gameObject.AddComponent<AudioSource>();
+                }
+
+                audioSource.clip = TriggerSound;
+                audioSource.volume = Volume;
+                audioSource.Play();
+
+                isPlayed = true;
+            }
+        }
+    }
+}
