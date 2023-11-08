bebf29fa44087dc2f6f3c619acc80cbb77f7ffe0
Yeshuwa <133879029+4nythng@users.noreply.github.com>
2023-11-09 07:26:47 +0800

    - Added 5th and 6th Floor Scene
    Added:
    > 5th and 6th Floor Scene
    > Scripts
        > Placard Object (Custom XR Grab Interactable)
        > Placard Socket (Custom XR Socket Interactor)
        > Placard Puzzle
        > Placard Shuffle (Spawner Alternative - Check Script for reason)
    > Models
        > Placards (Placeholder)
        > Placard Sockets (Placeholder)

---
+++ Assets/Scripts/Floor 5 & 6 - Placard/PlacardObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PlacardObject : XRGrabInteractable
+{
+    // This class exist to make the Placard unique for the Placard Sockets
+}
---
+++ Assets/Scripts/Floor 5 & 6 - Placard/PlacardPuzzle.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PlacardPuzzle : MonoBehaviour
+{
+    private List<XRSocketInteractor> sockets = new List<XRSocketInteractor>();
+    public bool isPuzzleComplete = false;
+
+    [Header("Game Objects")]
+    public XRSocketInteractor socket1;
+    public XRSocketInteractor socket2;
+    public XRSocketInteractor socket3;
+    public XRGrabInteractable placard1;
+    public XRGrabInteractable placard2;
+    public XRGrabInteractable placard3;
+
+    [Header("Level Progression")]
+    public GameObject DisableLevelLoop;
+
+    private Dictionary<XRSocketInteractor, XRBaseInteractable> socketPlacardPairs = new Dictionary<XRSocketInteractor, XRBaseInteractable>();
+
+    [System.Obsolete]
+    private void Start()
+    {
+        Debug.Log("PlacardPuzzle Start");
+        sockets.AddRange(GetComponentsInChildren<XRSocketInteractor>());
+
+        socketPlacardPairs.Add(socket1, placard1);
+        socketPlacardPairs.Add(socket2, placard2);
+        socketPlacardPairs.Add(socket3, placard3);
+
+        foreach (XRSocketInteractor socket in sockets)
+        {
+            socket.onSelectEnter.AddListener(OnObjectInserted);
+            socket.onSelectExit.AddListener(OnObjectRemoved);
+        }
+    }
+
+    [System.Obsolete]
+    private void OnObjectInserted(XRBaseInteractable interactable)
+    {
+        Debug.Log("OnObjectInserted: " + interactable.name);
+        CheckCondition();
+    }
+
+    [System.Obsolete]
+    private void OnObjectRemoved(XRBaseInteractable interactable)
+    {
+        Debug.Log("OnObjectRemoved: " + interactable.name);
+        CheckCondition();
+    }
+
+    [System.Obsolete]
+    private void CheckCondition()
+    {
+        bool allSocketsFilledCorrectly = true;
+
+        foreach (XRSocketInteractor socket in sockets)
+        {
+            if (!socketPlacardPairs.ContainsKey(socket))
+            {
+                continue;
+            }
+
+            XRBaseInteractable expectedPlacard = socketPlacardPairs[socket];
+
+            if (socket.selectTarget != expectedPlacard)
+            {
+                allSocketsFilledCorrectly = false;
+                break;
+            }
+        }
+
+        isPuzzleComplete = allSocketsFilledCorrectly;
+
+        if (isPuzzleComplete)
+        {
+            Debug.Log("Puzzle Complete!");
+            // Deactivate the GameObject when the puzzle is complete
+            if (DisableLevelLoop != null)
+            {
+                DisableLevelLoop.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.Log("Puzzle Incomplete.");
+            // You can handle other cases here
+        }
+    }
+}
