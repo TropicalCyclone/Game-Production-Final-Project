9540140d2ae65d64a3a94327d804e9ad5e467b5c
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-11-09 13:44:51 +0800

    Level-2 - Modified Loop Counter to handle events - Added Depricated to seperated floor levels so people dont use it anymore

--- Assets/Scripts/LoopCounter.cs
+++ Assets/Scripts/LoopCounter.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LoopCounter : MonoBehaviour
 {
+    [SerializeField] public List<NumCountEvent> events = new List<NumCountEvent>();
 
-    [SerializeField] int maxCount;
     private int _loopCount = -1;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             _loopCount += 1;
-            Debug.Log("Loop amount: " + _loopCount);
+            for(int i = 0; i < events.Count; i++)
+            {
+                if (events[i].LoopCount == _loopCount)
+                {
+                    events[i].ReachNum.Invoke();
+                }
+            }
         }
+    }
+}
+
+[System.Serializable]
+public class NumCountEvent
+{
+    [SerializeField] private string _name;
+    [SerializeField] private int _loopCount;
+
+    [SerializeField] private UnityEvent _reachNum;
+    public UnityEvent ReachNum { get { return _reachNum; } }
+
+    public int LoopCount
+    {
+        get { return _loopCount; }
     }
 }
