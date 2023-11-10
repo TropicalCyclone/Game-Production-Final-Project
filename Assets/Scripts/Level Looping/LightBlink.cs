666c71ea2434189ffc64636fbb46f6a11bf6ed7e
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-11-10 13:52:39 +0800

    floor-2 - added elevator animation and flashlight

--- Assets/Scripts/Level Looping/LightBlink.cs
+++ Assets/Scripts/Level Looping/LightBlink.cs
@@ -20,13 +20,27 @@ public class LightBlink : MonoBehaviour
 
     public void Blink()
     {
-        StartCoroutine(iBlink(0.5f, 0.2f));
+        StartCoroutine(iBlink(0.5f, 0.2f,3));
     }
 
-    IEnumerator iBlink(float time, float intensity)
+    public void SetLights(float intensity)
     {
         RenderSettings.ambientLight = new Color(RenderSettings.ambientLight.r * intensity, RenderSettings.ambientLight.g * intensity, RenderSettings.ambientLight.b * intensity);
-        yield return new WaitForSeconds(time);
-        RenderSettings.ambientLight = _lightColor;
+    }
+
+    public void LightsOff()
+    {
+        SetLights(0.1f);
+    }
+
+    IEnumerator iBlink(float time, float intensity, int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            SetLights(intensity);
+            yield return new WaitForSeconds(time);
+            RenderSettings.ambientLight = _lightColor;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
