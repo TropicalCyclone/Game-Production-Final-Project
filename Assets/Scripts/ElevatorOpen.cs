031a1dcab20fd07b72943de764b2c5aeb6f3e72e
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2023-11-12 01:13:22 +0800

    intro-elevator - added Elevator scene

---
+++ Assets/Scripts/ElevatorOpen.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ElevatorOpen : MonoBehaviour
+{
+
+    [SerializeField] private Animator Leftdoor;
+    [SerializeField] private Animator Rightdoor;
+
+    private bool _flashlightEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ElevatorDoorOpen()
+    {
+        Leftdoor.Play("DoorLOpen");
+        Rightdoor.Play("DoorROpen");
+    }
+
+    public void ElevatorDoorClose()
+    {
+        Leftdoor.Play("DoorLClose");
+        Rightdoor.Play("DoorRClose");
+    }
+
+    public void FlashlightOpen()
+    {
+        if (!_flashlightEvent)
+        {
+            ElevatorDoorClose();
+            _flashlightEvent = true;
+        }
+    }
+}
